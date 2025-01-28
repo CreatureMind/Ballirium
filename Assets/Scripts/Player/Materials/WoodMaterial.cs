0bd1d533f8d7b6b33593042a9fa8d0129f053ecd
L4zyScientist <korenbs99@gmail.com>
2025-01-28 19:00:23 +0200

    Update WoodMaterial.cs

--- Assets/Scripts/Player/Materials/WoodMaterial.cs
+++ Assets/Scripts/Player/Materials/WoodMaterial.cs
@@ -12,12 +12,11 @@ public class WoodMaterial : MonoBehaviour
     public float force = 1.5f;
     public float jumpForce = 3.5f;
     public Rigidbody _rigidbody;
-    
     public float _maxVelocity = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
