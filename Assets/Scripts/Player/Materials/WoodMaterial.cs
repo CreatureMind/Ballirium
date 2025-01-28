8c0574e4a90bd22d082856afd4a3bf674586df56
L4zyScientist <korenbs99@gmail.com>
2025-01-28 18:48:26 +0200

    woodmetirial

---
+++ Assets/Scripts/Player/Materials/WoodMaterial.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WoodMaterial : MonoBehaviour
+{
+    
+    // privates
+    private bool _isGrounded;
+    // publics
+    public float force = 1.5f;
+    public float jumpForce = 3.5f;
+    public Rigidbody _rigidbody;
+    
+    public float _maxVelocity = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
