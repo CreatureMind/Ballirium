034f4441e24f7b7c84cbfb1afab9838d034e70f4
CreatureMind <124280801+CreatureMind@users.noreply.github.com>
2025-02-23 16:04:48 +0200

    Squashed commit of the following:
    commit 06c3661bd04d02a94b93d7c43868196efb80613d
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Sat Feb 22 21:41:32 2025 +0200
    
        fixed platform script
    
        cleaning needed but the platform should work
    
    commit 3e8d5d9b34159d07bcf222e865e4225bf0424127
    Merge: 02ec2a1 91527ca
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Fri Feb 21 22:59:20 2025 +0200
    
        Merge branch 'main' into matan_dev
    
    commit 02ec2a184f9a8efbdd6dbb072d68b9bcdcb4fed0
    Merge: 605a3fe cf1d1bb
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Thu Feb 20 21:15:27 2025 +0200
    
        Merge branch 'main' into matan_dev
    
    commit 605a3feb530ffb8ab7a13b762e0f58b1bb1e0e7a
    Merge: 1af38a6 0a70374
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Thu Feb 20 17:16:40 2025 +0200
    
        Merge branch 'main' into matan_dev
    
    commit 1af38a649f8b12b7f99262bac90d3239089d7f6e
    Merge: 2233c23 6ef9a39
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Thu Feb 20 16:10:04 2025 +0200
    
        Merge branch 'main' into matan_dev
    
    commit 2233c23c21428ecadb7f4e5e38504e86b0a72e7a
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Mon Feb 17 22:12:25 2025 +0200
    
        updated platform
    
    commit 214400cf85d7b30c99e2f64aa3ae080f25f78933
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Sat Feb 15 21:09:59 2025 +0200
    
        Update MatanScene.unity
    
    commit 83e88093534825cefd29df2e04dc85ee0fa5c3ce
    Author: MatanBelfer <belfer.matan@gmail.com>
    Date:   Sat Feb 15 21:09:34 2025 +0200
    
        Update Platform.cs

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,17 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
-// TODO remove
-// public enum Direction
-// {
-//     Up,
-//     Down,
-//     North,
-//     South,
-//     East,
-//     West
-// }
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class Platform : MonoBehaviour
 {
@@ -21,61 +11,149 @@ public class Platform : MonoBehaviour
     public bool onRepeat;
     public bool movePlayer = true;
     public float startDelay;
-    public float waitTime;
+    public float waitTime = 10f;
     public float speed;
     public Vector3 startPosition;
     public Vector3 endPosition;
+    private Vector3 _target;
+
 
     private bool _CanMove = true;
+    private bool _isPlatformMoving;
+    private bool _returning;
     private GameObject _player = null;
+    private Rigidbody _playerRigidbody = null;
+    private Rigidbody _platformRigidbody = null;
     private bool _onPlatform;
+    private Vector3 _LastPos;
+    private Vector3 _CurrentPos;
+    private bool _enabled;
+
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
-        StartCoroutine(MovePlatform());
+        _target = endPosition;
+        _platformRigidbody = GetComponent<Rigidbody>();
     }
 
-    private IEnumerator MovePlatform()
+    private IEnumerator WaitPlatform()
     {
-        yield return new WaitForSeconds(startDelay);
-        while (onRepeat)
+        _enabled = true;
+        Debug.Log("1inside wait platform");
+        Debug.Log("1Target: " + _target);
+        _returning = !_returning;
+        _target = _target == startPosition ? endPosition : startPosition;
+        yield return new WaitForSeconds(waitTime);
+        _enabled = false;
+        Debug.Log("2Target: " + _target);
+        Debug.Log("2inside wait platform");
+  
+    }
+
+    //TODO CLEAN CODE
+    void FixedUpdate()
+    {
+        if (!_enabled)
         {
-            while (transform.position != endPosition)
+            if (Mathf.Abs((transform.position - _target).magnitude) > 0.1f)
             {
-                // _player.transform.position =
-                    // Vector3.MoveTowards(_player.transform.position, endPosition, speed * Time.deltaTime);
-
-                transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
-
-                yield return 0;
+                _isPlatformMoving = true;
+                Vector3 move = _target - transform.position;
+                move.Normalize();
+                _platformRigidbody.MovePosition(_platformRigidbody.position + move * (Time.deltaTime * speed));
             }
-
-            if (isReturn)
+            else
             {
-                yield return new WaitForSeconds(waitTime);
-                while (transform.position != startPosition)
-                {
-                    // _player.transform.position =
-                    //     Vector3.MoveTowards(_player.transform.position, endPosition, speed * Time.deltaTime);
-                    transform.position = Vector3.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
-                    yield return 0;
-                }
+                Debug.Log("Inside else from start to end ");
+                _isPlatformMoving = false;
+                StartCoroutine(WaitPlatform());
             }
-
-            yield return new WaitForSeconds(waitTime);
         }
+        // if (onRepeat)
+        // {
+        //     if (!_returning)
+        //     {
+        //         Debug.Log(">>>>>>>>>>>>>>");
+        //         if (Mathf.Abs((transform.position - _target).magnitude) > 1f)
+        //         {
+        //             _isPlatformMoving = true;
+        //             Vector3 move = endPosition - transform.position;
+        //             move.Normalize();
+        //             _platformRigidbody.MovePosition(_platformRigidbody.position + move * (Time.deltaTime * speed));
+        //         }
+        //         else
+        //         {
+        //             Debug.Log("Inside else from start to end ");
+        //             _isPlatformMoving = false;
+        //             StartCoroutine(WaitPlatform());
+        //         }
+        //     }
+        //     //
+        //     // else
+        //     // {
+        //     //     Debug.Log("<<<<<<<<<<<<<<<<<<<<<<");
+        //     //     if (isReturn)
+        //     //     {
+        //     //         Debug.Log("inside is return 71");
+        //     //         if (Mathf.Abs((transform.position - startPosition).magnitude) > 1f)
+        //     //         {
+        //     //             Debug.Log("inside 74");
+        //     //             _isPlatformMoving = true;
+        //     //             Vector3 move = startPosition - transform.position;
+        //     //             move.Normalize();
+        //     //             _platformRigidbody.MovePosition(_platformRigidbody.position + move * (Time.deltaTime * speed));
+        //     //         }
+        //     //         else
+        //     //         {
+        //     //             _isPlatformMoving = false;
+        //     //             StartCoroutine(WaitPlatform());
+        //     //         }
+        //     //     }
+        //     // }
+        // }
+
+
+        if (_onPlatform)
+        {
+            if (_isPlatformMoving)
+            {
+                Vector3 vel = _platformRigidbody.position - _LastPos;
+                Debug.Log("In fixed update:  vel   " + vel);
+                Debug.Log("In fixed update:  _platformRigidbody.velocity   " + _platformRigidbody.velocity);
+                Debug.Log("In fixed update:  _playerRigidbody.velocity   " + _playerRigidbody.velocity);
+                _LastPos = _platformRigidbody.position;
+                _playerRigidbody.AddForce(vel, ForceMode.Force);
+            }
+
+
+            // _player.transform.SetParent(transform);
+            // Vector3 vel = _platformRigidbody.position - _LastPos;
+            // _playerRigidbody.position = (_playerRigidbody.position + vel) * Time.deltaTime;
+            // _LastPos = _platformRigidbody.position;
+            // Debug.Log("In fixed update: " + _platformRigidbody.velocity + " | " + _playerRigidbody.velocity);
+            // _playerRigidbody.velocity *= _platformRigidbody.velocity.magnitude ;
+        } // todo https://www.reddit.com/r/Unity3D/comments/14h5zkx/comment/jp9rkaa/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+
+        // if (_platformRigidbody != null && _playerRigidbody != null)
+        // Debug.Log("In fixed update: " + _platformRigidbody.velocity + " | " + _playerRigidbody.velocity);
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        
         if (_player == null)
         {
-            _player = other.gameObject;
-            
+            _LastPos = new Vector3(0, 0, 0);
+            _onPlatform = true;
+            _player = other.collider.gameObject;
+            _playerRigidbody = _player.GetComponent<Rigidbody>();
+            Debug.Log("in OnCollisionEnter vel:  " + _playerRigidbody.velocity);
             // Debug.Log("Inside onCollision");
         }
     }
-    
+
+    private void OnCollisionExit(Collision other)
+    {
+        _onPlatform = false;
+    }
 }
