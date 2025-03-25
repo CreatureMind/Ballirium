8987ddffd6620ec38b0881f7cbafd81a823eb9d9
MatanBelfer <belfer.matan@gmail.com>
2025-03-25 10:03:48 +0200

    updated door trap

--- Assets/Scripts/Traps/TrapDoorway.cs
+++ Assets/Scripts/Traps/TrapDoorway.cs
@@ -1,15 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 public class TrapDoorway : MonoBehaviour
 {
-    private GameObject[] numberOfDoors;
-    
+    private BoxCollider[] numberOfDoors;
+    private Random rnd;
+
     // Start is called before the first frame update
     void Start()
     {
-        numberOfDoors = GetComponentsInChildren<GameObject>();
+        numberOfDoors = GetComponentsInChildren<BoxCollider>();
+        rnd = new Random();
     }
 
     // Update is called once per frame
